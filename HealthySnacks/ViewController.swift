fc7aaeec7f273e966011cb2b80c9e352dbb66f85
Cuizihan <mg20330012@smail.nju.edu.cn>
2020-09-26 10:46:39 +0800

    Initial Commit

---
+++ HealthySnacks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthySnacks
+//
+//  Created by CuiZihan on 2020/9/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
